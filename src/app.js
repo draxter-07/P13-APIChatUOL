6611c18f09749dcf2253df28cdd538ac25c49747
Philippe <philippe.idalgoprestes@gmail.com>
2023-06-30 10:32:10 -0300

    Implementação das funções; partindo para a implementação do MongoDB

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function remove(){
     users = newUsers;
 }
 
-setInterval(remove, 15000);
+//setInterval(remove, 15000);
 
 app.post('/participants', (req, res) => {
     const data = req.body;
@@ -34,7 +34,7 @@ app.post('/participants', (req, res) => {
     const expectedName = joi.string();
     // Faz a validação do userName
     const { error, value } = expectedName.validate(user);
-    if (error == undefined){
+    if (error == undefined && user != undefined){
         function right(name){
             const objMessage = {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: day().format('HH:mm:ss')};
             messages.push(objMessage);
@@ -64,4 +64,100 @@ app.post('/participants', (req, res) => {
 
 app.get('/participants', (req, res) => {
     res.send(users);
+})
+
+app.post('/messages', (req, res) =>{
+    const data = req.body;
+    const from = req.get('User');
+    // Valida o tipo dos dados
+    const toTextJoi = joi.string();
+    const typeJoi = joi.any().allow('message', 'private_message');
+    const fromJoi = joi.any().required();
+    const { errorTo, valueTo } = toTextJoi.validate(data.to);
+    const { errorText, valueText } = toTextJoi.validate(data.text);
+    const { errorType, valueType } = typeJoi.validate(data.type);
+    const { errorFrom, valueFrom } = fromJoi.validate(from);
+    // verifica se o from está na sala
+    let foundFrom = 0;
+    for (let a = 0; a < users.length; a++){
+        if (users[a].name == from){
+            foundFrom = 1;
+            break;
+        }
+    }
+    if (errorTo == undefined && errorText == undefined && errorType == undefined && errorFrom == undefined && foundFrom == 1){
+        // adicionar time e from para o objeto
+        let newObject = {from: from, to: data.to, text: data.text, type: data.type, time: day().format('HH:mm:ss')};
+        messages.push(newObject);
+        res.sendStatus(201);
+    }
+    else{
+        res.sendStatus(422);
+    }
+})
+
+app.get('/messages', (req, res) => {
+    const user = req.get('User');
+    const limit = req.query.limit;
+    const limitJoi = joi.number().integer().positive();
+    const userJoi = joi.string().required;
+    const { errorLimit, valueLimit } = limitJoi.validate(limit);
+    const { errorUser, valueUser } = userJoi.validate(user);
+    if (errorLimit == undefined && errorUser == undefined){
+        let messagesIt = [];
+        // Verificar todas as mensagens mandadas em Todos, para ele e por ele (privadas)
+        for (let a = 0; a < messages.length; a++){
+            if (messages[a].to == 'Todos' || messages[a].to == user || messages[a].from == user){
+                if (messagesIt.length == 0){
+                    messagesIt.push(messages[a]);
+                }
+                else{
+                    for (let b = 0; b < messagesIt.length; b++){
+                        if (messagesIt[b] == messages[a]){
+                            break;
+                        }
+                        else if (b == messagesIt.length - 1 && messagesIt[b] != messages[a]){
+                            messagesIt.push(messages[a]);
+                        }
+                    }
+                }
+            }
+        }
+        // Verificar se há limite
+        if (limit != undefined){
+            let messagesOutput = [];
+            for (let a = 0; a < limit; a++){
+                messagesOutput.push(messagesIt[messagesIt.length - a - 1]);
+            }
+            res.send(messagesOutput);
+        }
+        else{
+            res.send(messagesIt);
+        }
+    }
+    else {
+        res.sendStatus(422);
+    }
+})
+
+app.post('/status', (req, res) => {
+    const user = req.get('User');
+    // Valida o User e verifica se ele está em Users
+    const userJoi = joi.string().required();
+    const { errorUser, valueUser } = userJoi.validate(user);
+    let foundUser = -1;
+    for (let a = 0; a < users.length; a++){
+        if (users[a].name == user){
+            foundUser = a;
+            break;
+        }
+    }
+    if (errorUser == undefined && foundUser != -1){
+        //Atualiza o lastStatus
+        users[foundUser].lastStatus = Date.now();
+        res.sendStatus(200);
+    }
+    else{
+        res.sendStatus(404);
+    }
 })
